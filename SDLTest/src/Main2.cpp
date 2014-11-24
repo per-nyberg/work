b60af49b94f4376e2cc54611ee150532ad21337b
per-nyberg <per.nyberg0@gmail.com>
2014-11-24 14:44:21 +0100

    sdl2

---
+++ SDLTest/src/Main2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <SDL.h>
+#include <stdio.h>
+
+int main(){
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		std::cout << "Error initializing SDL" << SDL_GetError();
+		return 1;
+
+	SDL_Window *window = SDL_CreateWindow("123",100,100,600,800,SDL_WINDOW_SHOWN);
+
+	if(window == nullptr)
+		std::cout << "SDL Error creating window";
+		return 1;
+
+	SDL_Renderer *render  = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if(render == nullptr){
+		SDL_DestroyWindow(window);
+		SDL_DestroyRenderer(render);
+		std::cout << "Error creating renderer";
+		SDL_Quit();
+		return 1;
+	}
+
+	std::string image_path = sökväg till bild
+
+	SDL_Surface *bmp_surface = SDL_LoadBMP(image_path.c_str());
+	if(bmp_surface == nullptr){
+		SDL_DestroyRenderer(render);
+		SDL_DestroyWindow(window);
+		std::cout << "SDL error when importing bmp_surface";
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Texture* bmp_texture = SDL_CreateTextureFromSurface(render,bmp_surface);
+	SDL_FreeSurface(bmp_surface);
+
+	if(bmp_texture == nullptr){
+		SDL_DestroyRenderer(render);
+		SDL_DestroyWindow(window);
+		std::cout << "SDL error SDL_CreateTextureFromSurface";
+		SDL_Quit();
+		return 1;
+	}
+	//
+
+
+
+
+	SDL_Render_Clear(render); 	// tömmer videobufferten
+	SDL_RenderCopy(render,bmp_texture);
+	SDL_RenderPresent(render);
+
+	SDL_Delay(2000);
+
+	SDL_Quit();
+	return 0;
+}
+
--- SDLTest/src/Test.cpp
+++ SDLTest/src/Test.cpp
@@ -5,49 +5,60 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
-{
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
+int main( int argc, char* args[] ){
+	bool quit = false;
+	SDL_Event e;
+	while(!quit){	// game loop
+		while(SDL_PollEvent(&e) != 0){	// event loop
+			if(e.type == SDL_QUIT){		// if user exits
+				quit = true;
+			}
+		}
+	}
 
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+	return 0;
 
-    //Initialize SDL...
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
+	//The window we'll be rendering to
+	    /*SDL_Window* window = NULL;
 
-    else
-     {
-         //Create window
-         window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-         if( window == NULL )
-         {
-             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-         }
+	    //The surface contained by the window
+	    SDL_Surface* screenSurface = NULL;
 
-         else
-         {
-             //Get window surface
-             screenSurface = SDL_GetWindowSurface(window);
+	    //Initialize SDL...
+	    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	    {
+	        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+	    }
 
-             //Fill the surface white
-             SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+	    else
+	     {
+	         //Create window
+	         window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+	         if( window == NULL )
+	         {
+	             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+	         }
 
-             //Update the surface
-             SDL_UpdateWindowSurface(window);
+	         else
+	         {
+	             //Get window surface
+	             screenSurface = SDL_GetWindowSurface(window);
 
-             //Wait two seconds
-             SDL_Delay(2000);
-         }
-     }
-    //Destroy window
-    SDL_DestroyWindow(window);
+	             //Fill the surface white
+	             SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
 
-    //Quit SDL subsystems
-    SDL_Quit();
+	             //Update the surface
+	             SDL_UpdateWindowSurface(window);
 
-    return 0;
+	             //Wait two seconds
+	             SDL_Delay(2000);
+	         }
+	     }
+	    //Destroy window
+	    SDL_DestroyWindow(window);
+
+	    //Quit SDL subsystems
+	    SDL_Quit();
+		*/
+
 }
