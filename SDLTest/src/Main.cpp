70c8ef94b4f27906d9fbd8640939fcb5f75a338b
per-nyberg <per.nyberg0@gmail.com>
2014-11-26 15:37:36 +0100

    sdl2

--- SDLTest/src/Main.cpp
+++ SDLTest/src/Main.cpp
@@ -1,138 +1,83 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2013)
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
+#include <iostream>
 #include <SDL.h>
 #include <stdio.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+int main(){
 
-//Starts up SDL and creates window
-bool init();
+	/*void init(){		// init SDL and create window
 
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
-
-//The image we will load and show on the screen
-SDL_Surface* gXOut = NULL;
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-		success = false;
 	}
-	else
-	{
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-			success = false;
+	void close(){		// remove assets and quit SDL
+
+	}
+	void loadMedia(){	// load media
+
+	}*/
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		std::cout << "Error initializing SDL" << SDL_GetError();
+		return 1;
+
+	SDL_Window *window = SDL_CreateWindow("123",100,100,600,800,SDL_WINDOW_SHOWN);
+
+	if(window == nullptr)
+		std::cout << "SDL Error creating window";
+		SDL_Quit();
+		return 1;
+
+	SDL_Renderer *render  = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if(render == nullptr){
+		SDL_DestroyWindow(window);
+		SDL_DestroyRenderer(render);
+		std::cout << "Error creating renderer";
+		SDL_Quit();
+		return 1;
+	}
+
+	std::string image_path = "C:\\Users\\Per\\Downloads\\background_550_400.bmp";
+
+	SDL_Surface *bmp_surface = SDL_LoadBMP(image_path.c_str());
+	if(bmp_surface == nullptr){
+		SDL_DestroyRenderer(render);
+		SDL_DestroyWindow(window);
+		std::cout << "SDL error when importing bmp_surface";
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Texture* bmp_texture = SDL_CreateTextureFromSurface(render,bmp_surface);
+	SDL_FreeSurface(bmp_surface);
+
+	if(bmp_texture == nullptr){
+		SDL_DestroyRenderer(render);
+		SDL_DestroyWindow(window);
+		std::cout << "SDL error SDL_CreateTextureFromSurface";
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_RenderClear(render); 	// tömmer videobufferten
+	SDL_RenderCopy(render,bmp_texture,NULL,NULL);
+	SDL_RenderPresent(render);
+
+	// LOOP
+	bool running = true;
+	while(running){
+		SDL_Event event;
+		while(SDL_PollEvent(&event)){
+
 		}
-		else
-		{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
-		}
 	}
+	SDL_Delay(4000);
 
-	return success;
-}
 
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
 
-	//Load splash image
-	gXOut = SDL_LoadBMP( "03_event_driven_programming/x.bmp" );
-	if( gXOut == NULL )
-	{
-		printf( "Unable to load image %s! SDL Error: %s\n", "03_event_driven_programming/x.bmp", SDL_GetError() );
-		success = false;
-	}
-
-	return success;
-}
-
-void close()
-{
-	//Deallocate surface
-	SDL_FreeSurface( gXOut );
-	gXOut = NULL;
-
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-
-	//Quit SDL subsystems
+	// CLEAN UP
+	SDL_DestroyTexture(bmp_texture);
+	SDL_DestroyRenderer(render);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
-}
-
-int main( int argc, char* args[] )
-{
-	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
-			//While application is running
-			while( !quit )
-			{
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-				}
-
-				//Apply the image
-				SDL_BlitSurface( gXOut, NULL, gScreenSurface, NULL );
-
-				//Update the surface
-				SDL_UpdateWindowSurface( gWindow );
-			}
-		}
-	}
-
-	//Free resources and close SDL
-	close();
-
 	return 0;
 }
+
+
